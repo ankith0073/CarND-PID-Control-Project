e8c5553cec1c63f14fa120e31ea4e725ed9076e2
ankith0073 <ankith0073@gmail.com>
2017-08-23 12:32:33 +0200

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,6 @@
 #include "PID.h"
 
+
 using namespace std;
 
 /*
@@ -11,11 +12,27 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    PID::p_error = 0;
+    PID::i_error = 0;
+    PID::d_error = 0;
+    PID::tolerance = 0.2;
+    PID::twiddle_on = true;
+    PID::N = 100;
+
 }
 
 void PID::UpdateError(double cte) {
+    PID::d_error = cte - PID::p_error;
+    PID::i_error += cte;
+    PID::p_error = cte;
 }
 
 double PID::TotalError() {
+
 }
+
+
 
